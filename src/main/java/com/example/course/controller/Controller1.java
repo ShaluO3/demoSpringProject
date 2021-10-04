b0e6f4ec21b831c91438eb658c15eed75d61f461
rajat <rajatengineer25@gmail.com>
2021-10-04 12:58:08 +0530

    added delete put post api functionality

--- src/main/java/com/example/course/controller/Controller1.java
+++ src/main/java/com/example/course/controller/Controller1.java
@@ -6,6 +6,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +23,38 @@ public class Controller1
 	public List<Course> getResponse() 
 	{
 	   return cs.getAllCourses();
-   }
+    }
+	
+	
+	@GetMapping(value = "/courses/{id}")
+	public Course getCourse(@PathVariable String id) 
+	{
+	   return cs.getCourse(id);
+    }
+	
+	
+	
+	@RequestMapping( method = RequestMethod.POST ,value = "/courses")
+	public void addCourse(@RequestBody Course course) 
+	{
+	      cs.addCourse(course);
+    }
+	
+	
+	@RequestMapping( method = RequestMethod.PUT ,value = "/courses/{id}")
+	public void updateCourse(@RequestBody Course course , @PathVariable String id) 
+	{
+	      cs.updateCourse(course , id);
+    }
+	
+	
+	@RequestMapping( method = RequestMethod.DELETE ,value = "/courses/{id}")
+	public void deleteCourse(@PathVariable String id) 
+	{
+	    cs.deleteCourse(id);
+    }
+	
+	
+	
+	
 }
--- src/main/java/com/example/course/controller/CourseService.java
+++ src/main/java/com/example/course/controller/CourseService.java
@@ -1,5 +1,6 @@
 package com.example.course.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,14 +9,51 @@ import org.springframework.stereotype.Service;
 @Service
 public class CourseService 
 {
-	  private List<Course> courses = Arrays.asList(
+	  private List<Course> courses = new ArrayList<>( Arrays.asList(
 			   new Course("1" , "java" ,"This is java course"),
 			   new Course("2" , "javaScript" ,"This is javaScript course"),
 			   new Course("3" , "Android" ,"This is android course")
-			   ); 
+			   )); 
 	  
 	  public List<Course> getAllCourses(){
 		  return courses;
 	  }
+	  
+	  public Course getCourse(String id) {
+		  
+			 for(int i=0; i<courses.size();i++) {
+				 Course obj = courses.get(i);
+				 
+				 if(obj.getId().equals(id)) return obj;
+			 }
+			 return null;
+	  }
+
+	public void addCourse(Course course) {
+		courses.add(course);
+	}
+
+	
+	public void updateCourse(Course course, String id) {
+		
+		 for(int i=0; i<courses.size();i++) {
+			 Course obj = courses.get(i);
+			 
+			 if(obj.getId().equals(id)) {
+				 courses.set(i, course);
+				 return ;
+			 }
+		 }
+		
+	}
+
+	public void deleteCourse(String id) {
+		
+		for(int i=0; i<courses.size();i++) {
+			 Course obj = courses.get(i);
+			 
+			 if(obj.getId().equals(id)) courses.remove(i);
+		 }
+	}
 	
 }
